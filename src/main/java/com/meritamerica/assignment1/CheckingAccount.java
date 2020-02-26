2bd66945aec5d20db024cf14f79494faa872c84b
Kristin Skipper <kskipper.03@gmail.com>
2020-02-26 17:12:49 -0600

    new edits2

--- src/main/java/com/meritamerica/assignment1/CheckingAccount.java
+++ src/main/java/com/meritamerica/assignment1/CheckingAccount.java
@@ -1,5 +1,57 @@
 package com.meritamerica.assignment1;
 
-public class CheckingAccount {
+public class CheckingAccount{
+
+	double balance = 0;
+	double openingBalance = 0;
+	double interestRate = 0.01;
 	
-}
+	public CheckingAccount(double openingbalance) {
+			
+			this.balance = openingBalance;
+	}
+	
+	public double getBalance() {
+		
+		return balance;
+	
+	}
+	
+	public double getInterestRate() {
+		
+		return interestRate;
+	}
+	
+	public boolean withdraw(double amount) {
+		if (balance <= 0) {
+			System.out.println("Your balance is zero or below. Can't make withdrawal.");
+			return false;
+			
+		} else if (balance > 0) {
+			balance = balance - amount;
+			
+		}
+		return true;
+		
+		
+	}
+	
+	public boolean deposit(double amount) {
+		
+	}
+	
+	public double futureValue() {
+		//Formula: FV = PV(1 + interestRate) ^ years;
+		//double FV = balance * Math.pow(1.01, 3);
+		
+	}
+	
+	public String toString() {
+		return (
+					"Checking Account Balance: " + 
+				   "\nChecking Account Interest Rate: " + 
+					"\nChecking Account Balance in 3 Years: " 
+				   );
+	}
+	
+	
--- src/main/java/com/meritamerica/assignment1/MeritAmericaBankApp.java
+++ src/main/java/com/meritamerica/assignment1/MeritAmericaBankApp.java
@@ -2,4 +2,28 @@ package com.meritamerica.assignment1;
 
 public class MeritAmericaBankApp {
 	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Merit Bank");
+		
+		
+			
+				AccountHolder person1 = new AccountHolder("Jill", "Joy", "Smith", "7894563214", 100, 1000);
+				System.out.println(person1);
+				person1.setFirstname("Allison");
+				System.out.println(person1.getFirstName());
+				System.out.println("***********************");
+				
+				AccountHolder person2 = new AccountHolder("Jim", "Lee", "Williams", "564123951", 100, 1000);
+				System.out.println(person2);
+				
+				
+				
+//				person1.getCheckingAccount(2500);
+//				System.out.println(person1);
+				double value = 100 * Math.pow(1.01, 3);
+				System.out.println(value);
+		
+	}
+	
 }
